9f2dea859065b49033240878010e327b131f67e3
Hamza <hamzakhan174722@gmail.com>
2024-08-28 13:38:05 +0500

    add header

--- Blog/src/components/Header/Heade.jsx
+++ Blog/src/components/Header/Heade.jsx
@@ -5,13 +5,9 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-  return;
-}
-
-export default Header;
-
-/**const authStatus = useSelector((state) => state.auth.status);
-  const navigate = useNavigate();
+  // getting currect status
+  const authStatus = useSelector((state) => state.auth.state);
+  const nivigate = useNavigate();
 
   const navItems = [
     {
@@ -38,16 +34,51 @@ export default Header;
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
+    },
+  ];
 
+  return (
+    <>
+      <header className="py-3 shadow bg-gray-500">
+        <Container>
+          <nav className="flex">
+            <div className="mr-4">
+              <Link to="/">
+                <Logo width="70px" />
+              </Link>
 
+              <ul className="flex ml-auto">
+                {navItems.map((item) =>
+                  item.active ? (
+                    <li key={item.name}>
+                      <button
+                        onClick={() => nivigate(item.slug)}
+                        className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                      >
+                        {item.name}
+                      </button>
+                    </li>
+                  ) : null
+                )}
 
+                {/* if authStatus is true  thne show logout btn */}
+                {authStatus && (
+                  <li>
+                    <Logout_btn />
+                  </li>
+                )}
+              </ul>
+            </div>
+          </nav>
+        </Container>
+      </header>
+    </>
+  );
+}
 
+export default Header;
 
-
-
-
-
-      -----------------------------------
+/**
         <header className="py-3 shadow bg-gray-500">
       <Container>
         <nav className="flex">
