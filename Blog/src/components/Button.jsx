85ba5f3f10549d2495f1b702581ba302bae4479a
Hamza <hamzakhan174722@gmail.com>
2024-08-28 18:35:07 +0500

    add input

--- Blog/src/components/Button.jsx
+++ Blog/src/components/Button.jsx
@@ -10,7 +10,7 @@ export default function Button({
 }) {
   return (
     <button
-      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      className={`inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full ${bgColor} ${textColor} ${className}`}
       {...props}
     >
       {children}
