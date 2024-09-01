b5e9efc538b79f0c2d21dc1d9de0c185e98cabbe
Hamza <hamzakhan174722@gmail.com>
2024-09-01 12:03:00 +0500

    update loading page

--- Blog/src/components/Loading/Loading.jsx
+++ Blog/src/components/Loading/Loading.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <div className="flex flex-row gap-2">
-        <div className="w-4 h-4 rounded-full bg-purple-700 animate-bounce [animation-delay:.7s]"></div>
-        <div className="w-4 h-4 rounded-full bg-purple-700 animate-bounce [animation-delay:.3s]"></div>
-        <div className="w-4 h-4 rounded-full bg-purple-700 animate-bounce [animation-delay:.7s]"></div>
-      </div>
+    <div class="flex space-x-2 justify-center items-center bg-white h-screen ">
+      <span class="sr-only">Loading...</span>
+      <div class="h-8 w-8 bg-orange-700 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+      <div class="h-8 w-8 bg-orange-700 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+      <div class="h-8 w-8 bg-orange-700 rounded-full animate-bounce"></div>
     </div>
   );
 }
